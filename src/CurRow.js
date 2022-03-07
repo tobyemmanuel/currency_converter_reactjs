af43d6a0f071c26edf8faabc0b363545071a56d2
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-03-07 16:14:39 +0100

    Created app layout

---
+++ src/CurRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CurRow() {
+  return (
+    <div>
+        <input type="number" className='inputField'/>
+        <select>
+            <option value="Hi">Hi</option>
+        </select>
+    </div>
+  )
+}
