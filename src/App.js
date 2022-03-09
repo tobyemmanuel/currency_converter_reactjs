a2fbeb09902d5702755fc0b8acfe3ccad67b00e7
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-03-09 09:16:01 +0100

    Created functionality to determine exchange currncy values

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import CurRow from './CurRow';
-const API_URL = 'https://freecurrencyapi.net/api/v2/latest'
+const API_URL = 'https://api.currencyapi.com/v2/latest?apikey=itaKd1wemitPKoUVPur7jH6PrgDmA9QIXennUI7k'
 
 function App() {
+  const [CurOptions, setCurOptions] = useState([])
+  const [fromCur, setFromCur] = useState()
+  const [toCur, setToCur] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountFrom, setAmountFrom] = useState(true)
+  const [fxRate, setFxRate] = useState()
+ // console.log(fxRate)
+
+  let toAmount, fromAmount
+
+  if(amountFrom){
+    fromAmount = amount
+    toAmount = amount * fxRate
+  }else{
+    toAmount = amount
+    fromAmount = amount / fxRate
+  }
+  
+useEffect(()=> {
+    fetch(API_URL)
+    .then(dataResponse => dataResponse.json())
+    .then(data => {
+      const firstCur = Object.keys(data.data)[0]
+      setCurOptions([...Object.keys(data.data)])
+      setFromCur(data.query.base_currency)
+      setToCur(firstCur)
+      setFxRate(data.data[firstCur])
+    })
+}, [])
+    
+  function fromChangedAmount(e){
+    setAmount(e.target.value)
+    setAmountFrom(true)
+  }
+
+  function toChangedAmount(e){
+    setAmount(e.target.value)
+    setAmountFrom(false)
+  }
+
   return (
     <>
     <div className='cc-body'>
       <h1>Currency Converter</h1>
-      <CurRow />
+      <CurRow
+        selecttype = "curFrom"
+        setCur = {fromCur}
+        CurOptions = {CurOptions}
+        onChangeCur = {e=> setFromCur(e.target.value)}
+        amount = {fromAmount} 
+        onChangeAmount = {fromChangedAmount}
+      />
       <div className='equal-sign'>=</div>
-      <CurRow />
+      <CurRow 
+        selecttype = "curTo"
+        setCur = {toCur}
+        CurOptions = {CurOptions}
+        onChangeCur = {e=> setToCur(e.target.value)}
+        amount = {toAmount}
+        onChangeAmount = {toChangedAmount}
+      />
     </div>
     </>
   );
--- src/CurRow.js
+++ src/CurRow.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-export default function CurRow() {
+export default function CurRow(props) {
+    const {
+        CurOptions,
+        setCur,
+        onChangeCur,
+        selecttype,
+        amount,
+        onChangeAmount
+    } = props
+ 
+
+
   return (
+     
     <div>
-        <input type="number" className='inputField'/>
-        <select>
-            <option value="Hi">Hi</option>
+        <input type="number" className='inputField' value={amount} onChange={onChangeAmount}/>
+        <select id={selecttype} value={setCur} onChange={onChangeCur}>
+            {CurOptions.map(option=>(<option value={option} key={option}>{option}</option>))}
         </select>
     </div>
   )
